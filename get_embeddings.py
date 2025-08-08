d82de44f9ace43bce66c8c6bbf0a8149255a3c69
Yiyou <342065733@qq.com>
2025-08-07 21:04:38 -0700

    fix

---
+++ get_embeddings.py
@@ -0,0 +1,11 @@
+from transformers import AutoModelForCausalLM
+import torch, numpy as np
+
+model = AutoModelForCausalLM.from_pretrained(
+    "meta-llama/Llama-3.1-8B-Instruct",
+    torch_dtype="float16",
+    device_map="auto"
+)
+
+embeddings = model.get_input_embeddings().weight.detach().cpu().numpy()
+np.save("/home/weichenzhang/hallucination/SAT/utils/llama_embedding.npy", embeddings)
